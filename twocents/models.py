7b411fd22190b492af184aa002a71119d15145fa
prezadito <aditya4mvp@gmail.com>
2016-09-14 18:04:01 -0400

    create models in pseudocode

--- twocents/models.py
+++ twocents/models.py
@@ -3,3 +3,25 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Bill(models.Model):
+    title = models.charfield(max_length=500)
+    sponsored_by = models.charfield(max_length=64)
+    introduced = models.DateField()
+    summary = models.charfield(max_length=1000)
+    text = models.charfield(max_length=8000)
+    status
+    upvotes_downvotes
+
+
+class User(models.Model):
+    party_affiliation
+    location
+    history
+
+
+class Vote(models.Model):
+    comments
+    plus_minus
+    User_ID
+    Bill_ID
