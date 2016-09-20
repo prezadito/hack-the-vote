f100f6674c38bba5f555275b073f3c394de317de
prezadito <aditya4mvp@gmail.com>
2016-09-20 03:27:16 -0400

    create models, blank template and migrations

--- twocents/models.py
+++ twocents/models.py
@@ -1,27 +1,54 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
-class Bill(models.Model):
-    title = models.charfield(max_length=500)
-    sponsored_by = models.charfield(max_length=64)
-    introduced = models.DateField()
-    summary = models.charfield(max_length=1000)
-    text = models.charfield(max_length=8000)
-    status
-    upvotes_downvotes
-
-
 class User(models.Model):
-    party_affiliation
-    location
-    history
+    name = models.CharField(max_length=64)
+
+    DEMOCRAT = 'DEM'
+    REPUBLICAN = 'REP'
+    THIRD_PARTY = 'THIRD'
+    INDEPENDENT = 'IND'
+    PARTY_AFFILIATION_CHOICES = (
+        (DEMOCRAT, 'Democrat'),
+        (REPUBLICAN, 'Republican'),
+        (THIRD_PARTY, 'Third Party'),
+        (INDEPENDENT, 'Independent')
+    )
+
+    party_affiliation = models.CharField(
+        max_length = 2,
+        choices = PARTY_AFFILIATION_CHOICES,
+        default = INDEPENDENT
+    )
+
+    def __str__(self):
+        return self.name
+
+    # location
 
 
-class Vote(models.Model):
-    comments
-    plus_minus
-    User_ID
-    Bill_ID
+class Bill(models.Model):
+    name = models.CharField(max_length=64)
+    author = models.ForeignKey('auth.user')
+    proposed_date = models.DateTimeField(blank=True, null=True)
+    summary = models.CharField(max_length=148)
+    text = models.TextField()
+    userUpVotes = models.ManyToManyField(User, blank=True, related_name='billUpVotes')
+    userDownVotes = models.ManyToManyField(User, blank=True, related_name='billDownVotes')
+
+    def propose(self):
+        self.proposed_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+# class Vote(models.Model):
+#     comments
+#     plus_minus
+#     User_ID
+#     Bill_ID
--- twocents/views.py
+++ twocents/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def bill_list(request):
+    return render(request, 'twocents/bill_list.html', {})
